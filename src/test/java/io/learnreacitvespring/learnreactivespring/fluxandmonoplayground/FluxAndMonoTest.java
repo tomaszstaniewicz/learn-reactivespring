89e0f46ec52bdc12edc82313320810d6fa1870bb
Tomasz Staniewicz <tomasz.staniewicz@deviniti.com>
2019-04-30 10:20:45 +0200

    added flat map example

--- src/test/java/io/learnreacitvespring/learnreactivespring/fluxandmonoplayground/FluxAndMonoTest.java
+++ src/test/java/io/learnreacitvespring/learnreactivespring/fluxandmonoplayground/FluxAndMonoTest.java
@@ -6,6 +6,8 @@ import reactor.test.StepVerifier;
 
 import java.util.List;
 
+import static reactor.core.scheduler.Schedulers.parallel;
+
 public class FluxAndMonoTest {
 
 
@@ -38,4 +40,40 @@ public class FluxAndMonoTest {
                 .expectErrorMessage("exception message")
                 .verify();
     }
+
+    @Test
+    public void testFlatMap() {
+        Flux<String> flux = Flux.just("1", "2", "3", "4", "5", "6")
+                .flatMap(s -> Flux.fromIterable(createNewArray(s)))
+                .log();
+
+        StepVerifier.create(flux)
+                .expectNextCount(12)
+                .verifyComplete();
+
+    }
+
+    @Test
+    public void testFlatMap_parallel() {
+        Flux<String> flux = Flux.just("1", "2", "3", "4", "5", "6")
+                .window(2)
+                .flatMap((s) -> s.map(this::createNewArray).subscribeOn(parallel())
+                        .flatMap(Flux::fromIterable))
+                .log();
+
+        StepVerifier.create(flux)
+                .expectNextCount(12)
+                .verifyComplete();
+
+    }
+
+    private List<String> createNewArray(String s) {
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return List.of(s, "new value " + s);
+    }
+
 }
